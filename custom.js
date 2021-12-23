043b493e5b7ee523ff078d1db431e5646fea2655
Gearmo <93457954+Gearmo@users.noreply.github.com>
2021-12-22 21:07:20 -0800

    Add files via upload

---
+++ custom.js
@@ -0,0 +1,44 @@
+(function ($) {
+
+    $(document).ready(function () {
+
+        //nav bar function//
+        $(document).on('click', '.navbar-collapse.in', function (e) {
+            if ($(e.target).is('a') && $(e.target).attr('class') != 'dropdown-toggle') {
+                $(this).collapse('hide');
+            }
+        });
+
+        $('body').scrollspy({
+            target: '.navbar-collapse',
+            offset: 195
+        });
+
+        //smooth scroll from nav bar//
+        $('a.page-scroll').on("click", function (e) {
+            e.preventDefault();
+            var anchor = $(this);
+            $('html, body').stop().animate({
+                scrollTop: $(anchor.attr('href')).offset().top - 50
+            }, 1000);
+        });
+
+        //back to top scroll//        
+        $(document).ready(function(){ 
+            $(window).scroll(function(){ 
+                if ($(this).scrollTop() > 100) { 
+                    $('#scroll').fadeIn(); 
+                } else { 
+                    $('#scroll').fadeOut(); 
+                } 
+            }); 
+            $('#scroll').click(function(){ 
+                $("html, body").animate({ scrollTop: 0 }, 600); 
+                return false; 
+            }); 
+        });
+
+    });
+
+})(jQuery);
+
